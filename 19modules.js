866656d75b103205990c5920afd897aa3eda12a3
isha-150570 <isha150570@gmail.com>
2022-08-08 01:00:47 +0600

    second commit

--- 19modules.js
+++ 19modules.js
@@ -1,7 +1,7 @@
 /* // write a function which output multipication table by 13
 ekti function likhba jeta 13 er namoto (multiplication table)output hishabe dekhabe/ */
 
-
+/* 
 function namota(input){
   for (var i=1;i<=10;i++){
     var finalNamota=1*input;
@@ -9,4 +9,24 @@ function namota(input){
   }
   return finalNamota;
 }
-var result=namota(13);
+var result=namota(13); */
+
+
+
+
+/* ekta function likhba jeta j kono nam k uppercase ba regular case hishabe nibe ar output hishabe sey nam lowercase kore return korbe/ */
+
+
+
+
+/* function upperCaseToLowerCaseConverter(input){
+  var result =input.toLowerCase();
+  return result;
+}
+var text='isha';
+var finalResult=upperCaseToLowerCaseConverter(text);
+console.log(finalResult); */
+
+
+
+
